bd321322f0a0376be3e48c4dd374ab7fafc25fdb
adamczykjac <adamczyk.jac@gmail.com>
2014-02-20 19:36:33 +0100

    Core prototype

---
+++ app/controllers/pdfdocs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+ * Module dependencies
+ */
+
+//TO DO: PDFS
+
+//  var PDFDocument = require('pdfkit');
+
+// //Generuj Karta
+// exports.renderPDF = function() {
+//     res.render('index', {
+//         user: req.user ? JSON.stringify(req.user) : 'null'
+//     });
+// };
